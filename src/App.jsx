40c90f9e953266eddb0ad411bd710cd7592a8561
Taher_Shoker <taher.shoker@dexef.net>
2022-09-27 11:22:01 +0200

    create structure for app with typeScript and scss

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "shared/assets/styles/main.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="container">youTube Play List</div>
+    </div>
+  );
+}
+
+export default App;
