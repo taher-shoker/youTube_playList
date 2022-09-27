a51c68ce9a29ea5d01957ec8a25db3ecb0abe12d
Taher_Shoker <taher.shoker@dexef.net>
2022-09-27 12:36:21 +0200

    Add Header for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "containers/layout/header/header";
 import AppRoutes from "routes";
 import "shared/assets/styles/main.scss";
 
 function App() {
   return (
     <div className="app">
-    
+      <Header/>
       <AppRoutes />
     </div>
   );
